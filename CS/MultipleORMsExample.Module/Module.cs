3709392ce9342712a43497f69c0f42068eb47932
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ CS/MultipleORMsExample.Module/Module.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.EF;
+using MultipleORMsExample.Module.BusinessObjects;
+
+namespace MultipleORMsExample.Module {
+    public sealed partial class MultipleORMsExampleModule : ModuleBase {
+        public MultipleORMsExampleModule() {
+            InitializeComponent();
+            ExportedTypeHelpers.AddExportedTypeHelper(new EFExportedTypeHelperCF());
+            Database.SetInitializer<MyDbContext>(new DropCreateDatabaseIfModelChanges<MyDbContext>());
+        }
+        public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
+            ModuleUpdater updater = new DatabaseUpdate.Updater(objectSpace, versionFromDB);
+            return new ModuleUpdater[] { updater };
+        }
+    }
+}
---
+++ VB/MultipleORMsExample.Module/BusinessObjects/XpoSampleObject.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Linq
+Imports System.Text
+Imports DevExpress.Persistent.BaseImpl
+Imports DevExpress.Xpo
+Imports DevExpress.Persistent.Base
+Imports DevExpress.ExpressApp.DC
+
+Namespace MultipleORMsExample.Module.BusinessObjects
+    <DefaultClassOptions>
+    Public Class XpoSampleObject
+        Inherits BaseObject
+
+        Public Sub New(ByVal session As Session)
+            MyBase.New(session)
+        End Sub
+        Private _name As String
+        Public Property Name() As String
+            Get
+                Return _name
+            End Get
+            Set(ByVal value As String)
+                SetPropertyValue("Name", _name, value)
+            End Set
+        End Property
+        Private _description As String
+        <Size(SizeAttribute.Unlimited)>
+        Public Property Description() As String
+            Get
+                Return _description
+            End Get
+            Set(ByVal value As String)
+                SetPropertyValue("Description", _description, value)
+            End Set
+        End Property
+    End Class
+End Namespace
